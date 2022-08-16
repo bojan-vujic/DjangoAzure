c4ce8a3acd20536e362a0fd0c5978b66f46a8e28
bojan-vujic <bojan.vujic@yahoo.com>
2022-08-16 11:56:30 +0200

    Update views.py

--- files/views.py
+++ files/views.py
@@ -21,8 +21,9 @@ from django.conf import settings
 from django.http import HttpResponse, Http404
 from .models import File
 from django.core.files.storage import FileSystemStorage
+from django.http import FileResponse
 
-azure_connection_string = "DefaultEndpointsProtocol=https;AccountName=bojan01;AccountKey=43yMGDN6D2JG+DOxHCdll2HOrGgzacYWmDAYQgRhFwUBGgoA9T3FJF1qbRImUVsz7rdghbKGcDJj+AStJH2RjQ==;EndpointSuffix=core.windows.net"
+azure_connection_string = ""
 container_name = "myfiles"
 
 
@@ -30,7 +31,6 @@ def upload_to_azure(myfile, azure_connection_string, container_name):
     container = ContainerClient.from_connection_string(azure_connection_string, container_name)
     if not container.exists():
         container.create_container()
-
     try:
         with open("./myfiles/" + myfile, "rb") as f:
             container.get_blob_client(myfile).upload_blob(f)
@@ -38,13 +38,10 @@ def upload_to_azure(myfile, azure_connection_string, container_name):
     except:
         print(myfile, "is already uploaded to container", container_name)
 
-
 def index(request):
     return render(request, "files/index.html", {})
 
-
 date_string = timezone.localtime(timezone.now()).strftime("%Y-%m-%d %H:%M:%S")
-
 
 def upload_file(request):
     if request.method == 'POST' and request.FILES['file']:
@@ -58,44 +55,16 @@ def upload_file(request):
         for myfile in files:
             if myfile == file.name:
                 print("yes, I found the file", myfile)
-                upload_to_azure(myfile, azure_connection_string, container_name)
+                #upload_to_azure(myfile, azure_connection_string, container_name)
                 messages.success(
                     request, f"{file.name} was successfully uploaded")
                 return render(request, "files/upload_file.html", {})
     return render(request, "files/upload_file.html", {})
 
-
 def list_files(request):
     files = File.objects.all()
     context = {"files": files}
     return render(request, "files/list_files.html", context=context)
-
-
-# def download(request, id):
-#     obj = File.objects.get(file_id=id)
-#     filename = obj.model_attribute_name.path
-#     response = FileResponse(open(filename, 'rb'))
-#     return response
-
-
-import os
-from django.conf import settings
-from django.http import HttpResponse, Http404
-
-def delete_file(request,file_id):
-    file = models.File.objects.get(pk=file_id)
-    file.deleted = 1
-    file.save()
-    return redirect("list_files")
-
-from django.http import FileResponse
-
-def download(request, file_id):
-    obj = File.objects.get(id=file_id)
-    filename = obj.file.path
-    response = FileResponse(open(filename, 'rb'))
-    return response
-
 
 def download(request, file_id):
     obj = File.objects.get(id=file_id)
