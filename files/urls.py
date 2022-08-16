5e62148da2626931075b87dacc7e937403eff8d7
bojan-vujic <bojan.vujic@yahoo.com>
2022-08-16 11:56:54 +0200

    Update urls.py

--- files/urls.py
+++ files/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('upload_file/', views.upload_file, name="upload_file"),
     path('list_files/', views.list_files, name="list_files"),
-    path('delete_file/<int:file_id>/', views.delete_file, name="delete_file"),
     path('download/<int:file_id>/', views.download, name="download"),
 ]
 
